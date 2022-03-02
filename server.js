bd33bc41ef5eb4071a61a1ab8791a51288cba6c5
Mysuu <mysuu.work@gmail.com>
2022-03-02 19:03:43 +0700

    DATN

--- server.js
+++ server.js
@@ -7,10 +7,18 @@ const usersRoute = require('./routes/usersRoute')
 const bookingsRoute = require('./routes/bookingsRoute')
 
 app.use(express.json())
+const path = require('path')
 app.use('/api/rooms', roomsRoute)
 app.use('/api/users', usersRoute)
 app.use('/api/bookings', bookingsRoute)
 
 const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV == 'production') {
+    app.use('/', express.static('client/build'))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+    })
+}
 
 app.listen(port, () => console.log(`Node Server Started`))
